907be5e22fbe9ac63a5b9925d04f5270d0c444df
woohyeoung <dngud951108@gmail.com>
2022-11-16 23:14:03 +0900

    20221116, Add: 백준11650

---
+++ Algorithm/sort/B11650.java
@@ -0,0 +1,37 @@
+package Algorithm.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class B11650 {
+	public static void main(String[] args) {
+		BufferedReader br;
+		StringTokenizer stz;
+		StringBuilder sb = new StringBuilder();
+		try {
+		    br = new BufferedReader(new InputStreamReader(System.in));
+			int testcase = Integer.parseInt(br.readLine());
+			int[][] ary = new int[testcase][2];
+			for (int i = 0; i < testcase; i++) {
+				stz = new StringTokenizer(br.readLine());
+				ary[i][0] = Integer.parseInt(stz.nextToken());
+				ary[i][1] = Integer.parseInt(stz.nextToken());
+			}
+			Arrays.sort(ary, (o1, o2) -> {
+				if (o1[0] == o2[0]) {
+					return o1[1] - o2[1];
+				} else {
+					return o1[0] - o2[0];
+				}
+			});
+			for (int i = 0; i < testcase; i++) {
+				sb.append(ary[i][0]).append(' ').append(ary[i][1]).append('\n');
+			}
+			System.out.println(sb);
+		} catch (IOException e) { e.printStackTrace(); }
+	}
+}
