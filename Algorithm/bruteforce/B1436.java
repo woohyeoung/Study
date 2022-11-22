ee02577e1f4e373f60953a09e646a371cf0dd662
woohyeoung <dngud951108@gmail.com>
2022-11-22 21:57:47 +0900

    20221122, Fix: 백준1436

--- Algorithm/bruteforce/B1436.java
+++ Algorithm/bruteforce/B1436.java
@@ -20,4 +20,69 @@ public class B1436 {
 			System.out.println(--num);
 		} catch (IOException e) { e.printStackTrace(); }
 	}
+
+	// 범위를 좁혀 속도 개선
+	static void func(int n) {
+		int count = 1;
+		int prev_digit = 0;
+		int num = 0;
+		while (true) {
+			if (((prev_digit % 10000) / 10) == 666 && prev_digit % 10 != 6) {
+				for (int i = 0; i < 1000; i++) {
+					if (count == n) {
+						System.out.print(prev_digit * 1000 + num);
+						return;
+					}
+					num++;
+					count++;
+				}
+				prev_digit++;
+			}
+			else if (prev_digit % 1000 == 666) {
+				num = 0;
+				for (int i = 0; i < 1000; i++) {
+					if (count == n) {
+						System.out.print(prev_digit * 1000 + num);
+						return;
+					}
+					count++;
+					num++;
+				}
+				prev_digit++;
+			}
+			else if (prev_digit % 100 == 66) {
+				num = 600;
+				for (int i = 0; i < 100; i++) {
+					if (count == n) {
+						System.out.print(prev_digit * 1000 + num);
+						return;
+					}
+					count++;
+					num++;
+				}
+				prev_digit++;
+			}
+			else if (prev_digit % 10 == 6) {
+				num = 660;
+				for (int i = 0; i < 10; i++) {
+					if (count == n) {
+						System.out.print(prev_digit * 1000 + num);
+						return;
+					}
+					num++;
+					count++;
+				}
+				prev_digit++;
+			}
+			else {
+				num = 666;
+				if (count == n) {
+					System.out.print(prev_digit * 1000 + num);
+					return;
+				}
+				count++;
+				prev_digit++;
+			}
+		}
+	}
 }
